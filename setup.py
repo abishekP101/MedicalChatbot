7cbd7428be2544d207ee82f838425f8f84ecdb7b
Abishek <abishek.123.prasad@gmail.com>
2025-08-15 11:12:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MedicalChatbot',
+    version='0.1.0',
+    author='Abishek Prasad',
+    author_email="abishek.456.prasad@gmail.com",
+    description='A medical chatbot application using LangChain and Pinecone',
+    packages=find_packages(),
+    install_requires=[]
+)
